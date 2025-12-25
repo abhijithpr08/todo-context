a4306a0d955a76fb6fe2eccaf18f5a7c80ac0dbc
abhijithpr08 <abhith628@gmail.com>
2025-12-25 23:39:23 +0530

    use useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-
+import { TodoContext,TodoProvider } from "./context/TodoContext"
 
 function App(){
   return(
-    <>
+    <TodoProvider>
+      <TodoContext/>
       <h1>hey todo</h1>
-    </>
+    </TodoProvider>
   )
 }
 
--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -3,8 +3,17 @@ import { Children, createContext, useEffect, useState } from "react"
 export const TodoContext = createContext();
 
 export const TodoProvider = ({ Children }) => {
+    console.log("hey")
     const [todos, setTodo] = (() => {
         const storedTodo = localStorage.getItem("todos");
         return storedTodo ? JSON.parse(storedTodo) : []
     })
+
+    useEffect(()=>{
+        localStorage.setItem("todos",JSON.stringify("todos"))
+    },[todos])
+
+    return <>
+    <h1><i>hey </i></h1>
+    </>
 }
